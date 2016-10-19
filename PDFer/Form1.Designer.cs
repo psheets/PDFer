1479158e59738a7fb43d432c89c9315767c2a85f
Phil Sheets <psheets88@gmail.com>
2016-10-19 12:17:04 -0400

    Add project files.

---
+++ PDFer/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace PDFer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.browse_source = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.browser_destination = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_PDF = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(335, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // browse_source
+            // 
+            this.browse_source.Location = new System.Drawing.Point(449, 12);
+            this.browse_source.Name = "browse_source";
+            this.browse_source.Size = new System.Drawing.Size(75, 23);
+            this.browse_source.TabIndex = 1;
+            this.browse_source.Text = "Browse";
+            this.browse_source.UseVisualStyleBackColor = true;
+            this.browse_source.Click += new System.EventHandler(this.browse_source_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 70);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(552, 316);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.TabStop = false;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // browser_destination
+            // 
+            this.browser_destination.Location = new System.Drawing.Point(449, 41);
+            this.browser_destination.Name = "browser_destination";
+            this.browser_destination.Size = new System.Drawing.Size(75, 23);
+            this.browser_destination.TabIndex = 4;
+            this.browser_destination.Text = "Browse";
+            this.browser_destination.UseVisualStyleBackColor = true;
+            this.browser_destination.Click += new System.EventHandler(this.browser_destination_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(335, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Source Folder";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Destination Folder";
+            // 
+            // btn_PDF
+            // 
+            this.btn_PDF.Location = new System.Drawing.Point(368, 402);
+            this.btn_PDF.Name = "btn_PDF";
+            this.btn_PDF.Size = new System.Drawing.Size(75, 23);
+            this.btn_PDF.TabIndex = 5;
+            this.btn_PDF.Text = "PDF";
+            this.btn_PDF.UseVisualStyleBackColor = true;
+            this.btn_PDF.Click += new System.EventHandler(this.btn_PDF_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(449, 402);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(75, 23);
+            this.btn_close.TabIndex = 6;
+            this.btn_close.Text = "Close";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 402);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(339, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(530, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.add_folder);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(582, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.RemoveFile);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 436);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_PDF);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.browser_destination);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.browse_source);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "PDFer";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            //throw new System.NotImplementedException();
+            return;
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button browse_source;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button browser_destination;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_PDF;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ PDFer/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+
+namespace PDFer
+{
+    public partial class Form1 : Form
+    {
+        BindingList<System.IO.FileInfo> fileList = new BindingList<System.IO.FileInfo>();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void add_folder(object sender, EventArgs e)
+        {
+              UpdateFileList();
+        }
+        private void browse_source_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog openFileDialog1 = new FolderBrowserDialog();
+            openFileDialog1.ShowNewFolderButton = false;
+            openFileDialog1.SelectedPath = textBox1.Text;
+
+            DialogResult result = openFileDialog1.ShowDialog();
+          
+            if (result == DialogResult.OK)
+            {
+
+                string source = openFileDialog1.SelectedPath;
+                textBox1.Text = source;                
+            }
+        }
+
+        private void browser_destination_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog openFileDialog1 = new FolderBrowserDialog();
+            openFileDialog1.ShowNewFolderButton = true;
+            openFileDialog1.SelectedPath = textBox1.Text;
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+
+                string source = openFileDialog1.SelectedPath;
+                textBox2.Text = source;
+                
+
+            }
+        }
+
+        private void btn_PDF_Click(object sender, EventArgs e)
+        {
+            PdfFiles();
+        }
+
+        // Close app when button is clicked
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        // Use Word automation to PDF files in Directorys and out put to destination.
+        private void PdfFiles()
+        {
+
+            // Create oject for not needed values
+            object oMissing = System.Reflection.Missing.Value;
+
+            var count = 0;
+
+            // Get 
+            DirectoryInfo dirInfo = new DirectoryInfo(textBox1.Text);
+            var wordFiles = dirInfo.GetFiles("*.doc").Where(name => !name.Name.StartsWith("~"));
+
+
+            // Create a new Word application object
+            Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = wordFiles.Count();
+
+
+            foreach (FileInfo wordFile in wordFiles)
+            {
+                //progress.Text = "PDFed " + count + " of " + wordFiles.Count();
+                
+                Object filename = (object)wordFile.FullName;
+                Object outputfilename;
+
+                if (wordFile.Name.EndsWith(".docx"))
+                {
+                    outputfilename = textBox2.Text + @"\" + wordFile.Name.Replace(".docx", ".pdf");
+                }
+
+                else
+                {
+                    
+                    outputfilename = textBox2.Text + @"\" + wordFile.Name.Replace(".doc", ".pdf");
+
+                }
+                
+
+                Document doc = word.Documents.Open(ref filename, ReadOnly:true);
+                doc.Activate();
+                doc.SaveAs(ref outputfilename, WdSaveFormat.wdFormatPDF);
+
+                object saveChanges = WdSaveOptions.wdDoNotSaveChanges;
+                ((_Document)doc).Close();
+                doc = null;
+                count++;
+                progressBar1.Value = count;
+            }
+
+            ((_Application)word).Quit(ref oMissing, ref oMissing, ref oMissing);
+            word = null;
+
+            if (count == wordFiles.Count())
+            {
+                MessageBox.Show("PDFing has completed!");
+                count = 0;
+                progressBar1.Value = count;
+                listBox1.DataSource = null;
+                listBox1.Items.Clear();
+            }
+        }
+
+        private void UpdateFileList()
+        {
+            // Get Files in directory and populate listBox1
+            DirectoryInfo dirInfo = new DirectoryInfo(textBox1.Text);
+            var temp = dirInfo.GetFiles("*.doc", SearchOption.AllDirectories).Where(name => !name.Name.StartsWith("~")).ToList();
+            foreach (var item in temp)
+            {
+                fileList.Add(item);
+            }
+            listBox1.DataSource = fileList;
+            Console.WriteLine(fileList.ToString());
+        }
+        private void RemoveFile(object sender, EventArgs e)
+        {
+            var index = fileList.Select(Name => listBox1.SelectedItem).ToString();
+            Console.WriteLine(index);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+        private void Form1_Resize(object sender, System.EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+                Hide();
+        }
+    }
+}
